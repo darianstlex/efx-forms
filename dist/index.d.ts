139aa92e3ef7505f36b9731b8b115eb9f4489919
darianstlex <iv.uzun@gmail.com>
2021-12-23 13:40:04 +0000

    export types

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,4 @@
 export { domain, truthyFy, shapeFy, $truthyFy, $shapeFy } from './utils';
 export { getForm } from './form';
 export { REfxForm, REfxField } from './react';
-export type { IFormValues, TFieldValue, TFormErrors, TFieldValidator } from './model';
+export type { IFormValues, IFormValidations, TFormErrors, TFieldValue, TFieldValidator, } from './model';
