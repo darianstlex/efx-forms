0c42fb81d6e6024b683d509880a96b34881c4eee
darianstlex <iv.uzun@gmail.com>
2021-12-24 10:08:49 +0000

    fix exports

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,2 @@
 export { domain, truthyFy, shapeFy, truthyFyStore, shapeFyStore } from './utils';
 export { getForm } from './form';
-export { REfxForm, REfxField } from './react';
-export * as validators from './validators';
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 export { domain, truthyFy, shapeFy, truthyFyStore, shapeFyStore } from './utils';
 export { getForm } from './form';
-export { REfxForm, REfxField } from './react';
-export * as validators from './validators';
 
 export type {
   IFormValues,
