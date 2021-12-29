f3134e3a8ad69b5f3118e9f17df994c16b655c6c
darianstlex <iv.uzun@gmail.com>
2021-12-29 16:17:52 +0000

    fix validation messages

--- src/validators.ts
+++ src/validators.ts
@@ -6,19 +6,19 @@ export const required = ({ msg = 'This field is required' } = {}): TFieldValidat
 export const email = ({ msg = 'Must be a valid email' } = {}): TFieldValidator =>
   (val: string) => !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(val) ? msg : false;
 
-export const min = ({ value, msg = `Must be greater than or equal to ${value}` }): TFieldValidator =>
+export const min = ({ value, msg = `Must be greater or equal to ${value}` }): TFieldValidator =>
   (val: string | number) => Number(val) < value ? msg : false;
 
-export const max = ({ value, msg = `Must be less than or equal to ${value}` }): TFieldValidator =>
+export const max = ({ value, msg = `Must be less or equal to ${value}` }): TFieldValidator =>
   (val: string | number) => Number(val) > value ? msg : false;
 
-export const lessThan = ({ value, msg = `Must be less than ${value}` }): TFieldValidator =>
+export const lessThan = ({ value, msg = `Length must be less than ${value}` }): TFieldValidator =>
   (val: string) => `${val}`.length >= value ? msg : false;
 
-export const moreThan = ({ value, msg = `Must be greater than ${value}` }): TFieldValidator =>
+export const moreThan = ({ value, msg = `Length must be more than ${value}` }): TFieldValidator =>
   (val: string) => `${val}`.length <= value ? msg : false;
 
-export const length = ({ value, msg = `Must be exactly ${value} characters` }): TFieldValidator =>
+export const length = ({ value, msg = `Length must be exactly ${value} characters` }): TFieldValidator =>
   (val: string | number) => `${val}`.length !== value ? msg : false;
 
 export const matches = ({ regexp, label = '', msg = `Must match the following: "${label}"`}): TFieldValidator =>
