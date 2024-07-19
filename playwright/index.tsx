2a10cf4e2eabbc49f34c082e9d38be1f8904b059
Ivan Uzun <ivan@motorway.co.uk>
2024-07-19 17:26:29 +0100

    test: add scope provider

--- playwright/index.tsx
+++ playwright/index.tsx
@@ -1,1 +1,10 @@
 import './index.css';
+
+import { beforeMount } from '@playwright/experimental-ct-react/hooks';
+import { Provider } from 'effector-react';
+import { fork } from 'effector';
+
+beforeMount(async ({ App }) => {
+  const scope = fork();
+  return <Provider value={scope}><App /></Provider>;
+});
