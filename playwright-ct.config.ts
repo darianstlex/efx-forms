20d7f554a568801e1fd773d682c51abb9fcbe3ed
Ivan Uzun <ivan@motorway.co.uk>
2024-07-22 22:02:40 +0100

    chore: tests config

--- playwright-ct.config.ts
+++ playwright-ct.config.ts
@@ -5,6 +5,7 @@ import { defineConfig, devices } from '@playwright/experimental-ct-react';
  */
 export default defineConfig({
   testDir: './',
+  outputDir: 'playwright/test-results',
   /* The base directory, relative to the config file, for snapshot files created with toMatchSnapshot and toHaveScreenshot. */
   snapshotDir: './__snapshots__',
   /* Maximum time one test can run for. */
@@ -18,7 +19,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['html', { outputFolder: 'playwright/report' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
--- src/tests/validators.spec.ts
+++ src/tests/validators.spec.ts
@@ -18,11 +18,11 @@ test.describe('Validators', async () => {
   test.describe('required', async () => {
     test('default message', async () => {
       const validate = required();
-      expect(validate('')).toBe('This field is required');
+      expect(validate('')).toEqual('This field is required');
     });
     test('custom message', async () => {
       const validate = required({ msg: 'Required!' });
-      expect(validate('')).toBe('Required!');
+      expect(validate('')).toEqual('Required!');
     });
     test('ok', async () => {
       const validate = required();
@@ -33,11 +33,11 @@ test.describe('Validators', async () => {
   test.describe('email', async () => {
     test('default message', async () => {
       const validate = email();
-      expect(validate('')).toBe('Must be a valid email');
+      expect(validate('')).toEqual('Must be a valid email');
     });
     test('custom message', async () => {
       const validate = email({ msg: 'Should be valid' });
-      expect(validate('test@test')).toBe('Should be valid');
+      expect(validate('test@test')).toEqual('Should be valid');
     });
     test('ok', async () => {
       const validate = email();
@@ -48,13 +48,13 @@ test.describe('Validators', async () => {
   test.describe('min', async () => {
     test('default message', async () => {
       const validate = min({ value: 20 });
-      expect(validate('')).toBe('Must be greater or equal to 20');
-      expect(validate(0)).toBe('Must be greater or equal to 20');
+      expect(validate('')).toEqual('Must be greater or equal to 20');
+      expect(validate(0)).toEqual('Must be greater or equal to 20');
     });
     test('custom message', async () => {
       const validate = min({ value: 20, msg: 'Add more' });
-      expect(validate('5')).toBe('Add more');
-      expect(validate(5)).toBe('Add more');
+      expect(validate('5')).toEqual('Add more');
+      expect(validate(5)).toEqual('Add more');
     });
     test('ok', async () => {
       const validate = min({ value: 20 });
@@ -66,13 +66,13 @@ test.describe('Validators', async () => {
   test.describe('max', async () => {
     test('default message', async () => {
       const validate = max({ value: 20 });
-      expect(validate('50')).toBe('Must be less or equal to 20');
-      expect(validate(50)).toBe('Must be less or equal to 20');
+      expect(validate('50')).toEqual('Must be less or equal to 20');
+      expect(validate(50)).toEqual('Must be less or equal to 20');
     });
     test('custom message', async () => {
       const validate = max({ value: 20, msg: 'Less' });
-      expect(validate('50')).toBe('Less');
-      expect(validate(50)).toBe('Less');
+      expect(validate('50')).toEqual('Less');
+      expect(validate(50)).toEqual('Less');
     });
     test('ok', async () => {
       const validate = max({ value: 20 });
@@ -84,11 +84,11 @@ test.describe('Validators', async () => {
   test.describe('lessThan', async () => {
     test('default message', async () => {
       const validate = lessThan({ value: 5 });
-      expect(validate('faraday')).toBe('Length must be less than 5');
+      expect(validate('faraday')).toEqual('Length must be less than 5');
     });
     test('custom message', async () => {
       const validate = lessThan({ value: 5, msg: 'Too long' });
-      expect(validate('faraday')).toBe('Too long');
+      expect(validate('faraday')).toEqual('Too long');
     });
     test('ok', async () => {
       const validate = lessThan({ value: 5 });
@@ -99,11 +99,11 @@ test.describe('Validators', async () => {
   test.describe('moreThan', async () => {
     test('default message', async () => {
       const validate = moreThan({ value: 3 });
-      expect(validate('de')).toBe('Length must be more than 3');
+      expect(validate('de')).toEqual('Length must be more than 3');
     });
     test('custom message', async () => {
       const validate = moreThan({ value: 3, msg: 'Too short' });
-      expect(validate('de')).toBe('Too short');
+      expect(validate('de')).toEqual('Too short');
     });
     test('ok', async () => {
       const validate = moreThan({ value: 3 });
@@ -114,11 +114,11 @@ test.describe('Validators', async () => {
   test.describe('length', async () => {
     test('default message', async () => {
       const validate = length({ value: 3 });
-      expect(validate('de')).toBe('Length must be exactly 3 characters');
+      expect(validate('de')).toEqual('Length must be exactly 3 characters');
     });
     test('custom message', async () => {
       const validate = length({ value: 3, msg: 'Nope' });
-      expect(validate('de')).toBe('Nope');
+      expect(validate('de')).toEqual('Nope');
     });
     test('ok', async () => {
       const validate = length({ value: 3 });
@@ -129,11 +129,11 @@ test.describe('Validators', async () => {
   test.describe('matches', async () => {
     test('default message', async () => {
       const validate = matches({ regexp: /some\dnum/, label: 'some0num' });
-      expect(validate('test')).toBe('Must match the following: \'some0num\'');
+      expect(validate('test')).toEqual('Must match the following: \'some0num\'');
     });
     test('custom message', async () => {
       const validate = matches({ regexp: /some\dnum/, msg: 'Nope' });
-      expect(validate('test')).toBe('Nope');
+      expect(validate('test')).toEqual('Nope');
     });
     test('ok', async () => {
       const validate = matches({ regexp: /some\dnum/ });
@@ -144,11 +144,11 @@ test.describe('Validators', async () => {
   test.describe('positive', async () => {
     test('default message', async () => {
       const validate = positive();
-      expect(validate(-2)).toBe('Must be a positive number');
+      expect(validate(-2)).toEqual('Must be a positive number');
     });
     test('custom message', async () => {
       const validate = positive({ msg: 'Nope' });
-      expect(validate(-1)).toBe('Nope');
+      expect(validate(-1)).toEqual('Nope');
     });
     test('ok', async () => {
       const validate = positive();
@@ -159,11 +159,11 @@ test.describe('Validators', async () => {
   test.describe('negative', async () => {
     test('default message', async () => {
       const validate = negative();
-      expect(validate(2)).toBe('Must be a negative number');
+      expect(validate(2)).toEqual('Must be a negative number');
     });
     test('custom message', async () => {
       const validate = negative({ msg: 'Nope' });
-      expect(validate(1)).toBe('Nope');
+      expect(validate(1)).toEqual('Nope');
     });
     test('ok', async () => {
       const validate = negative();
@@ -174,11 +174,11 @@ test.describe('Validators', async () => {
   test.describe('number', async () => {
     test('default message', async () => {
       const validate = number();
-      expect(validate('tete')).toBe('Must be a number');
+      expect(validate('tete')).toEqual('Must be a number');
     });
     test('custom message', async () => {
       const validate = number({ msg: 'Nope' });
-      expect(validate('we')).toBe('Nope');
+      expect(validate('we')).toEqual('Nope');
     });
     test('ok', async () => {
       const validate = number();
